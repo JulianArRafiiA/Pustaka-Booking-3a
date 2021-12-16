0ae64ee14625dfdc14df245ad66e4a9f48fe1050
JulianArRafiiA <91243132+JulianArRafiiA@users.noreply.github.com>
2021-12-16 11:11:00 +0700

    Pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"<br>Nama saya Julian Ar Rafii Achmad</br>,
+            Saya tinggal di Bekasi Timur,
+            Saya suka Olahraga";  
+    }
+}
