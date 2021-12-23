95f12a301960f445964107bd8cb41d62348d28f9
JulianArRafiiA <91243132+JulianArRafiiA@users.noreply.github.com>
2021-12-23 13:05:33 +0700

    Add files via upload

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Julian</li>
+        <li>Nama Tengah : Ar Rafii</li>
+        <li>Nama Belakang : Achmad</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="circle">
+        <li> Jalan Candikian No. 11 Mutiara Gading Timur</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="circle">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Olahraga</li>
+        <li>Belajar apa saja</li>
+    </ul>
+</section>
